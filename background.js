730d30d89bcf7c743087702a947bcab2d03b8577
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-09-15 16:40:37 -0700

    Run update monitor from background worker

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 'use strict';
-importScripts('service-worker.js', 'pomodoro-worker.js');
+importScripts('service-worker.js', 'pomodoro-worker.js', 'update-monitor.js');
 
 chrome.action.onClicked.addListener(async tab => {
   if (!tab?.id || !/^(https?|file):/i.test(tab.url || '')) return;
