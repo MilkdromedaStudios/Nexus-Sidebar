33ba3f4791f762cc9be7f676ee5047741c54e23e
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-09-15 16:36:24 -0700

    Load refined styles when Nexus is recovered from toolbar

--- background.js
+++ background.js
@@ -1,2 +1,9 @@
 'use strict';
 importScripts('service-worker.js', 'pomodoro-worker.js');
+
+chrome.action.onClicked.addListener(async tab => {
+  if (!tab?.id || !/^(https?|file):/i.test(tab.url || '')) return;
+  try {
+    await chrome.scripting.insertCSS({ target: { tabId: tab.id }, files: ['styles/refine.css'] });
+  } catch {}
+});
