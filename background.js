f12cf80cfd3d1f653e688ed4a2182f3c1e432161
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-09-15 16:41:12 -0700

    Load update UI on toolbar recovery

--- background.js
+++ background.js
@@ -5,5 +5,6 @@ chrome.action.onClicked.addListener(async tab => {
   if (!tab?.id || !/^(https?|file):/i.test(tab.url || '')) return;
   try {
     await chrome.scripting.insertCSS({ target: { tabId: tab.id }, files: ['styles/refine.css'] });
+    await chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['src/update-ui.js'] });
   } catch {}
 });
